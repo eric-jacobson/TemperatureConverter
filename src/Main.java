3ee674fc9987b79a390db96c783a0c8e6136a9af
eric-jacobson <ec-jacobson@hotmail.com>
2018-04-28 23:10:14 -0600

    Added buttons and text box to window and and AlertBox class

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -12,8 +14,6 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     Stage window;
-
-    Button button = new Button("Convert");
 
     public static void main(String[] args){
         launch(args);
@@ -26,6 +26,23 @@ public class Main extends Application {
 
         GridPane layout = new GridPane();
         layout.setPadding(new Insets(10,10,10,10));
+        layout.setVgap(8);
+        layout.setHgap(10);
+
+        Label tempLabel = new Label("Temperature:");
+        tempLabel.setStyle("-fx-text-fill: #E8E8E8");
+        GridPane.setConstraints(tempLabel, 0, 2);
+
+        TextField tempInput = new TextField();
+        GridPane.setConstraints(tempInput, 1, 2);
+
+        Button celsiusButton = new Button("Convert to C");
+        GridPane.setConstraints(celsiusButton, 20, 2);
+
+        Button fahrenheitButton = new Button("Convert to F");
+        GridPane.setConstraints(fahrenheitButton, 21, 2);
+
+        layout.getChildren().addAll(tempLabel, tempInput, celsiusButton, fahrenheitButton);
 
         Scene scene = new Scene(layout, 800, 100);
         scene.getStylesheets().add("WindowStyle.css");
